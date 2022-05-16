e2526d74f52e33da089a507d9e495523b89cd497
Alejandro Ariaz <ariaz39@hotmail.com>
2022-05-15 23:37:54 -0500

    Montaje de plantilla de admin

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('Admin.dashboard');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
