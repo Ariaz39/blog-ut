088bf0cd96965543e9a74c3b1e3768702b16674c
Alejandro Ariaz <33878604+Ariaz39@users.noreply.github.com>
2022-05-21 13:35:29 -0500

    routeWeb

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,25 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\Admin\AutorController;
-// use App\Http\Controllers\Admin\BlogController;
-// use App\Http\Controllers\Admin\CategoryController;
-// use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AutorController;
+use App\Http\Controllers\Admin\BlogController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
 
-// Route::get('/', function () {
-//    return view('Admin.login');
-<<<<<<< HEAD
-// });
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-// Route::group(['prefix' => 'admin'], function ()
-// {
-//    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::group(['prefix' => 'autor'], function () {
+        Route::get('/', [AutorController::class, 'index'])->name('autors');
+    });
 
-//    Route::group(['prefix' => 'category'], function ()
-//    {
-//        Route::get('/list-all', [CategoryController::class, 'index'])->name('categories');
-//        Route::get('/create-category', [CategoryController::class, 'create'])->name('create-category');
-//    });
+    Route::group(['prefix' => 'blog'], function () {
+        Route::get('/', [BlogController::class, 'index'])->name('blogs');
+    });
 
-//    Route::group(['prefix' => 'autor'], function ()
-//    {
-//        Route::get('/list-all', [AutorController::class, 'index'])->name('autors');
-//        Route::get('/create-autor', [AutorController::class, 'create'])->name('create-autor');
-//    });
-
-//    Route::group(['prefix' => 'blog'], function ()
-//    {
-=======
-//});
-//
-//Route::group(['prefix' => 'admin'], function () {
-//    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-//
-//    Route::group(['prefix' => 'category'], function () {
-//        Route::get('/list-all', [CategoryController::class, 'index'])->name('categories');
-//        Route::get('/create-category', [CategoryController::class, 'create'])->name('create-category');
-//    });
-//
-//    Route::group(['prefix' => 'autor'], function () {
-//        Route::get('/list-all', [AutorController::class, 'index'])->name('autors');
-//        Route::get('/create-autor', [AutorController::class, 'create'])->name('create-autor');
-//    });
-//
-//    Route::group(['prefix' => 'blog'], function () {
->>>>>>> 66e5cddbfaed48e0eb0f9b2d79c7efb6574bad5c
-//        Route::get('/list-all', [BlogController::class, 'index'])->name('blogs');
-//        Route::get('/create-blog', [BlogController::class, 'create'])->name('create-blog');
-//    });
-// });
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('categories');
+    });
+});
 
 
